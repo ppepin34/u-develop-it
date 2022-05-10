e931946d4e5ccd9a60267141baa2f64825f994b3
Phil Pepin <ppepin34@gmail.com>
2022-05-09 23:02:04 -0400

    does gitignore work?

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'm!92dRtkM31y',
+        database: 'election'
+    },
+    console.log('Connected to the election data.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+})
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
